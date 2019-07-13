51b575eca0248e55152d0836c4db386246e73fe8
EstevaoCostaG3 <estevao_carvalho.10@hotmail.com>
2019-07-13 18:17:07 -0400

    execution of utilities-installer.sh enabled + directory of vimrc fixed

--- set-up-config-files.sh
+++ set-up-config-files.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-for DOT_FILE in config-files/.*; do
+for DOT_FILE in ./config-files/.*; do
 	cp $DOT_FILE ~/
 done
 for DOT_DESKTOP in config-files/*.desktop; do
 	cp $DOT_DESKTOP ~/.config/autostart/
 done
-cp vimrc /usr/share/vim/
+cp ./config-files/vimrc /usr/share/vim/
