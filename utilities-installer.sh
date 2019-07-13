4c7ec9ea2a20a8c3904a2f503479c47e7118deed
EstevaoCostaG3 <estevao_carvalho.10@hotmail.com>
2019-07-13 18:20:49 -0400

    git user email and name autosetup added

--- utilities-installer.sh
+++ utilities-installer.sh
@@ -7,4 +7,6 @@ apt install -y vim zsh git htop screenfetch weka inxi tmux cmatrix nitrogen cmak
 sed -i '/^estevao/ s/\/bin\/bash/\/bin\/zsh/' /etc/passwd
 # install Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
+# set up github:
+git config --global user.email "estevao_carvalho.10@hotmail.com"
+git config --global user.name "EstevaoCostaG3"
