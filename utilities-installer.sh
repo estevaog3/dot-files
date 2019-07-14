f4e9d68ef1b0dfac535b347c73d9b396f46bd6e5
EstevaoCostaG3 <estevao_carvalho.10@hotmail.com>
2019-07-13 22:48:23 -0400

    ohmyzsh command installer changed + do-custom-tweaks added

--- utilities-installer.sh
+++ utilities-installer.sh
@@ -6,7 +6,7 @@ apt install -y vim zsh git htop screenfetch weka inxi tmux cmatrix nitrogen cmak
 # set zsh as default shell
 sed -i '/^estevao/ s/\/bin\/bash/\/bin\/zsh/' /etc/passwd
 # install Oh My Zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 # set up github:
 git config --global user.email "estevao_carvalho.10@hotmail.com"
 git config --global user.name "EstevaoCostaG3"
