96253c22bbe5daeccbc66495f1aa73229754ed6f
EstevaoCostaG3 <estevao_carvalho.10@hotmail.com>
2019-07-13 23:04:16 -0400

    fixed dot files copying

--- set-up-config-files.sh
+++ set-up-config-files.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-for DOT_FILE in ./config-files/.*; do
-	cp $DOT_FILE ~/
+DOT_FILES=$(ls ./config-files/ -a | egrep '^\.[A-Za-z]+')
+for DOT_FILE in $DOT_FILES; do
+	cp config-files/$DOT_FILE ~/
 done
-for DOT_DESKTOP in config-files/*.desktop; do
-	cp $DOT_DESKTOP ~/.config/autostart/
+DOT_DESKTOP_FILES=$(ls ./config-files/ -a | egrep '.desktop$')
+for DOT_DESKTOP in $DOT_DESKTOP_FILES; do
+	cp config-files/$DOT_DESKTOP ~/.config/autostart/
 done
 cp ./config-files/vimrc /usr/share/vim/
