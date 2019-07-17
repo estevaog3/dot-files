afe814c4d86eee8c4bf4c50587ede650a25fef07
EstevaoCostaG3 <estevao_carvalho.10@hotmail.com>
2019-07-16 21:59:47 -0400

    links created for dot and dot desktop files

--- set-up-config-files.sh
+++ set-up-config-files.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 DOT_FILES=$(ls config-files/ -a | egrep '^\.[A-Za-z]+')
 for DOT_FILE in $DOT_FILES; do
-	cp -r config-files/$DOT_FILE ~/
+	ln -f config-files/$DOT_FILE ~/
 done
+mkdir ~/.scripts
+ln -f config-files/.scripts/* ~/.scripts
 DOT_DESKTOP_FILES=$(ls config-files/ -a | egrep '.desktop$')
 for DOT_DESKTOP in $DOT_DESKTOP_FILES; do
-	cp config-files/$DOT_DESKTOP ~/.config/autostart/
+	ln -f config-files/$DOT_DESKTOP ~/.config/autostart/
 done
 cp config-files/vimrc /usr/share/vim/
