41734cab902eccc4bfdb91bced40509a392e950e
EstevaoCostaG3 <estevao_carvalho.10@hotmail.com>
2019-07-16 20:11:32 -0400

    minor change

--- set-up-config-files.sh
+++ set-up-config-files.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-DOT_FILES=$(ls ./config-files/ -a | egrep '^\.[A-Za-z]+')
+DOT_FILES=$(ls config-files/ -a | egrep '^\.[A-Za-z]+')
 for DOT_FILE in $DOT_FILES; do
 	cp -r config-files/$DOT_FILE ~/
 done
-DOT_DESKTOP_FILES=$(ls ./config-files/ -a | egrep '.desktop$')
+DOT_DESKTOP_FILES=$(ls config-files/ -a | egrep '.desktop$')
 for DOT_DESKTOP in $DOT_DESKTOP_FILES; do
 	cp config-files/$DOT_DESKTOP ~/.config/autostart/
 done
-cp ./config-files/vimrc /usr/share/vim/
+cp config-files/vimrc /usr/share/vim/
