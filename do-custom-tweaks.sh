c9e87bdd24ba8a7087c3a43a23ce3d3ac95ac576
EstevaoCostaG3 <estevao_carvalho.10@hotmail.com>
2019-07-16 20:18:18 -0400

    tweak added: show battery percentage

--- do-custom-tweaks.sh
+++ do-custom-tweaks.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #todo: custom keybindings with gsettings firefox, calculator and settings
-#todo: disable bluetooth by default
-#todo: show battery percentage
+#show battery percentage on dock:
+dconf write /org/gnome/desktop/interface/show-battery-percentage true
