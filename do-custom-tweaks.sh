dc8e8917d19acf297db6ed88e45e56edc972f303
EstevaoCostaG3 <estevao_carvalho.10@hotmail.com>
2019-07-16 21:38:06 -0400

    custom tweaks done by loading dconf file

--- do-custom-tweaks.sh
+++ do-custom-tweaks.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-#todo: custom keybindings with gsettings firefox, calculator and settings
-#show battery percentage on dock:
-dconf write /org/gnome/desktop/interface/show-battery-percentage true
+cat config-files/dconf-settings.ini | dconf load /
