197055e93da723e45aee48c5b9329524f31527d7
EstevaoCostaG3 <estevao_carvalho.10@hotmail.com>
2019-12-29 17:16:03 -0400

    Move configuration of zsh to take place after it's installation

--- scripts/install-programs.sh
+++ scripts/install-programs.sh
@@ -3,10 +3,10 @@
 # install basic utilities
 
 apt install -y vim zsh git htop screenfetch weka inxi tmux cmatrix nitrogen cmake virtualbox-qt 
-# set zsh as default shell
-sed -i '/^estevao/ s/\/bin\/bash/\/bin\/zsh/' /etc/passwd
 # install Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# set zsh as default shell
+sed -i '/^estevao/ s/\/bin\/bash/\/bin\/zsh/' /etc/passwd
 # set up github:
 git config --global user.email "estevao_carvalho.10@hotmail.com"
 git config --global user.name "EstevaoCostaG3"
