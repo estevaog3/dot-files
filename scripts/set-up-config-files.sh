51eea1832804c006fc447ca6a2474d6dd4f99751
EstevaoCostaG3 <estevao_carvalho.10@hotmail.com>
2019-12-29 15:06:10 -0400

    Update scripts/set-up-config-files.sh

--- scripts/set-up-config-files.sh
+++ scripts/set-up-config-files.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 BASE_DIR=..
 for DOT_FILE in $BASE_DIR/config-files/.*; do
-	cp $DOT_FILE ~/
+	if [ -f $DOT_FILE ]; then
+		cp $DOT_FILE ~/
+	fi
 done
 for DOT_DESKTOP in $BASE_DIR/config-files/*.desktop; do
 	cp $DOT_DESKTOP ~/.config/autostart/
 done
+
+cp -r $BASE_DIR/config-files/.scripts ~/
+cp -r $BASE_DIR/favorite-wallpapers ~/Pictures
