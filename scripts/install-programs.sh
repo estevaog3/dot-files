dafc7505f7028fe87e3891c128ad5e6d72e00f66
EstevaoCostaG3 <estevao_carvalho.10@hotmail.com>
2019-12-30 12:11:08 -0400

    Add curl to install; Add pathogen to install

--- scripts/install-programs.sh
+++ scripts/install-programs.sh
@@ -5,7 +5,7 @@ apt install -y vim zsh htop inxi tmux cmatrix  virtualbox-qt tree neofetch cowsa
 # media:
 apt install -y byzanz ffmpeg nitrogen
 # dev:
-apt install -y git cmake openssh-server nodejs npm yarn mysql-server postgresql mongodb-org docker-ce docker-ce-cli containerd.io ack-grep
+apt install -y git cmake openssh-server nodejs npm yarn mysql-server postgresql mongodb-org docker-ce docker-ce-cli containerd.io ack-grep curl
 # important:
 apt install -y timeshift clamav gufw needrestart debsecan lynis
 # install Oh My Zsh
@@ -15,3 +15,6 @@ sed -i '/^estevao/ s/\/bin\/bash/\/bin\/zsh/' /etc/passwd
 # set up github:
 git config --global user.email "estevao_carvalho.10@hotmail.com"
 git config --global user.name "EstevaoCostaG3"
+# install pathogen (vim plugin manager)
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+	curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
