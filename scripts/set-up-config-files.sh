cf0b0ee22a1baac94b13736580df00e4993c8117
EstevaoCostaG3 <estevao_carvalho.10@hotmail.com>
2019-12-30 12:17:27 -0400

    Add vim plugins

--- scripts/set-up-config-files.sh
+++ scripts/set-up-config-files.sh
@@ -11,3 +11,10 @@ done
 
 cp -r $BASE_DIR/config-files/.scripts ~/
 cp -r $BASE_DIR/favorite-wallpapers ~/Pictures
+
+# Add vim plugins
+cd ~/.vim/bundle
+git clone https://github.com/junegunn/goyo.vim.git
+git clone https://github.com/tpope/vim-commentary.git
+git clone https://github.com/tpope/vim-surround.git
+
