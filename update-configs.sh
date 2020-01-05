c5694201bf1a6c9246a31a701fec7ad0b57c495e
EstevaoCostaG3 <estevao_carvalho.10@hotmail.com>
2020-01-04 23:50:52 -0400

    Revert "Add script to update edited config files [see more]"
    The updates to the config files should be done manually.
    
    This reverts commit 6ab70552d8c5b21c5cee7dca19fffd15d63310f4.

--- update-configs.sh
+++
@@ -1,7 +0,0 @@
-N_FILES=`cat ./.paths | wc -l`
-for i in {1..$N_FILES}; do
-	PATH_AND_FILE=`cat ./.paths | awk -v line=$i '{if(NR == line) {print $0;}}'`
-	echo $PATH_AND_FILE
-	NAME=`echo $PATH_AND_FILE | awk -F"/" '{print $NF}'`
-	cp $PATH_AND_FILE ./config-files/$NAME
-done
