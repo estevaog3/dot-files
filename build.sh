7996f6db95b1460344e0c3a06008b3e70c2e3175
EstevaoCostaG3 <estevao_carvalho.10@hotmail.com>
2020-07-04 18:11:13 -0400

    Refactor: turn 'install-programs' into a function in build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,29 @@
 set -eu -o pipefail
 echo $USER > ./username
-./scripts/install-programs.sh
+
+installer(){
+	# utilities:
+	sudo apt install -y vim zsh htop inxi tmux cmatrix  virtualbox-qt tree neofetch cowsay xclip xdotool
+	# media:
+	sudo apt install -y ffmpeg nitrogen
+	# dev:
+	sudo apt install -y git cmake openssh-server nodejs npm yarn mysql-server postgresql mongodb-org ack-grep curl
+	# need to install timeshift:
+	sudo apt-add-repository -y ppa:teejee2008/ppa
+	sudo apt update
+	# important:
+	sudo apt install -y timeshift clamav gufw needrestart debsecan lynis
+	# install Oh My Zsh
+	curl -Lo install.sh https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+	sed -i 's/exec zsh/#/' install.sh
+	sudo sh install.sh
+	# set zsh as default shell
+	sudo sed -i "/^$USER/ s/\/bin\/bash/\/bin\/zsh/" /etc/passwd
+	# install pathogen (vim plugin manager)
+	sudo mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+			curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+}
+
+installer
 ./scripts/set-up-config-files.sh
 rm ./username
